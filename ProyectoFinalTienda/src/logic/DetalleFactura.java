ab91ebcc7e97068798f23221e88347dfb678e8bc
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-07-28 13:59:08 -0400

    changed property from String facturaCodigo to int facturaId

--- ProyectoFinalTienda/src/logic/DetalleFactura.java
+++ ProyectoFinalTienda/src/logic/DetalleFactura.java
@@ -2,30 +2,80 @@ package logic;
 
 public class DetalleFactura {
 	private int id;
-	private String facturaCodigo ;
+	private int facturaId ;
 	private Componente componente;
 	private double descuento; 
 	private int cantidadVendida ;
 	private double total ;
 	
-	public DetalleFactura(String facturaCodigo, Componente componente, double descuento, int cantidadVendida,
+	public DetalleFactura(int facturaId, Componente componente, double descuento, int cantidadVendida,
 			double total) {
 		super();
-		this.facturaCodigo = facturaCodigo;
+		this.facturaId = facturaId;
 		this.componente = componente;
 		this.descuento = descuento;
 		this.cantidadVendida = cantidadVendida;
 		this.total = total;
 	}
 
-	public DetalleFactura(int id, String facturaCodigo, Componente componente, double descuento, int cantidadVendida,
+	public DetalleFactura(int id, int facturaId, Componente componente, double descuento, int cantidadVendida,
 			double total) {
 		super();
 		this.id = id;
-		this.facturaCodigo = facturaCodigo;
+		this.facturaId = facturaId;
 		this.componente = componente;
 		this.descuento = descuento;
 		this.cantidadVendida = cantidadVendida;
 		this.total = total;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getFacturaId() {
+		return facturaId;
+	}
+
+	public void setFacturaId(int facturaId) {
+		this.facturaId = facturaId;
+	}
+
+	public Componente getComponente() {
+		return componente;
+	}
+
+	public void setComponente(Componente componente) {
+		this.componente = componente;
+	}
+
+	public double getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(double descuento) {
+		this.descuento = descuento;
+	}
+
+	public int getCantidadVendida() {
+		return cantidadVendida;
+	}
+
+	public void setCantidadVendida(int cantidadVendida) {
+		this.cantidadVendida = cantidadVendida;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+	
 }
